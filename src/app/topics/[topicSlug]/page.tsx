7d5cf91629dc90cd3a9b5ea9fe26fe2cd8e37bcd
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2024-01-21 16:48:53 +0000

    add elements to topicSlug page

--- src/app/topics/[topicSlug]/page.tsx
+++ src/app/topics/[topicSlug]/page.tsx
@@ -1,3 +1,5 @@
+import { Divider } from "@nextui-org/react";
+
 type Params = {
   params: {
     topicSlug: string;
@@ -5,6 +7,21 @@ type Params = {
 };
 
 const ShowSingleTopicPage = ({ params }: Params) => {
-  return <div>Topics Slug Page: {params.topicSlug}</div>;
+  const topicSlug = `${params.topicSlug.slice(0, 1).toUpperCase() + params.topicSlug.slice(1)}`;
+
+  return (
+    <section className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3 border rounded">
+        <h1 className="text-2xl font-bold mb-2">Topics: {topicSlug}</h1>
+      </div>
+
+      <aside className="text-center flex flex-col gap-4">
+        <article className="border shadow py-2 px-2 rounded-lg">
+          <h3 className="text-xl">Topics</h3>
+          <Divider className="my-2"></Divider>
+        </article>
+      </aside>
+    </section>
+  );
 };
 export default ShowSingleTopicPage;
